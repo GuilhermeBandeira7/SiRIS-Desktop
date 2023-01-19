d72d7242145ab58624ab4d328180d13ec3c51e26
Guilherme Bandeira <guilhermeprestes1@hotmail.com>
2023-01-19 13:19:31 -0300

    Interface de login implementada

---
+++ View/UserControls/SessionsControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using EntityMtwServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SiRISApp.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SessionsControl.xaml
+    /// </summary>
+    public partial class SessionsControl : UserControl
+    {
+        public Session Session
+        {
+            get { return (Session)GetValue(SessionProperty); }
+            set { SetValue(SessionProperty, value); }
+        }
+
+        public static readonly DependencyProperty SessionProperty = DependencyProperty.Register("Session", typeof(Session), typeof(SessionsControl), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SessionsControl sessionsControls = (SessionsControl)d;
+
+            if (sessionsControls != null)
+            {
+                sessionsControls.DataContext = sessionsControls.Session;
+            }
+        }
+
+
+        public SessionsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModel/Commands/RefreshSessionsCommand.cs
@@ -0,0 +1,40 @@
+﻿using EntityMtwServer.Entities;
+using MTWServerApiClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SiRISApp.ViewModel.Commands
+{
+    public class RefreshSessionsCommand : ICommand
+    {
+        public SiRISVm ViewModel { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RefreshSessionsCommand(SiRISVm vm)
+        {
+            ViewModel = vm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter == null)
+                return false;
+            return (parameter as User).Id > 0;
+        }
+
+        public void Execute(object? parameter)
+        {
+            ViewModel.GetSessions();
+        }
+    }
+}
